6e775adfc216e5ee1af26808d947ac94e557215a
ss-relick <ss-relick>
2021-03-07 18:36:07 +0900

    first commit

--- MajorLibrarySample/ViewController.swift
+++ MajorLibrarySample/ViewController.swift
@@ -2,18 +2,42 @@
 //  ViewController.swift
 //  MajorLibrarySample
 //
-//  Created by Shunsuke Suzuki on 2021/03/07.
+//  Created by Shunsuke Suzuki on 2021/03/06.
 //
 
 import UIKit
+import Alamofire
+import RxCocoa
+import RxSwift
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var testButton: UIButton!
+    @IBOutlet weak var rxLabel: UILabel!
+    let disposeBag = DisposeBag()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // RxSwift observe resultLabel text changed
+        resultLabel.rx.observe(String.self, "text")
+            .subscribe(onNext: { text in
+                // when resultLabel changed
+                self.rxLabel.text = "resultLabel changed to \(String(describing: text!))"
+            })
+            .disposed(by: disposeBag)
     }
 
-
+    @IBAction func touchTestButton(_ sender: Any) {
+        // Http Request And Decode Response to Httpbin
+        AF.request("https://httpbin.org/get").responseDecodable(of:Httpbin.self) { response in
+            self.resultLabel.text = response.value?.url
+        }
+    }
 }
-
+// MARK: - Httpbin
+// "https://httpbin.org/get" response
+// This site can create Codable struct from response
+// https://app.quicktype.io/
+struct Httpbin: Codable {
+    let origin: String
+    let url: String
+}
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '14.0'
+
+target 'MajorLibrarySample' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MajorLibrarySample
+  pod 'RxSwift', '6.1.0'
+  pod 'RxCocoa', '6.1.0'  
+  pod 'Alamofire', '~> 5.2'
+  pod 'IBAnimatable'
+
+end
